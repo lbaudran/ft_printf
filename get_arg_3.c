5c853844388c6c73c46e7d67558872a58830d72f
Lucas BAUDRANT <lbaudran@e3r6p10.42.fr>
2016-04-14 18:15:53 +0200

    troncature g

--- get_arg_3.c
+++ get_arg_3.c
@@ -6,7 +6,7 @@
 /*   By: lbaudran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/14 15:19:07 by lbaudran          #+#    #+#             */
-/*   Updated: 2016/04/14 18:03:02 by lbaudran         ###   ########.fr       */
+/*   Updated: 2016/04/14 18:15:21 by lbaudran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,20 @@ char		*get_shorter(va_list *arg, char *buffer, int *tab)
 	double	i;
 	int		n;
 	double	y;
+	int		a;
 
 	n = 0;
 	i = va_arg((*arg), double);
 	y = i;
 	if (y)
 		n = size_forscient(&y);
-	if (n > 6)
+	if (n > 5)
 	{
-		buffer = ft_dtoa(y, 6);
+		buffer = ft_dtoa(y, 5);
 		buffer = ft_round(buffer, ft_strlen(buffer) - 1);
+		a = ft_strlen(buffer) - 1;
+		while (buffer[a] == '0')
+			buffer[a--] = '\0';
 		buffer = arg_forscient(buffer, n);
 		return (buffer);
 	}
@@ -52,7 +56,11 @@ char		*get_shorter_up(va_list *arg, char *buffer, int *tab)
 	n = 0;
 	buffer = get_shorter(arg, buffer, tab);
 	while(buffer[n])
-		ft_toupper(buffer[n++]);
+	{
+		buffer[n] = ft_toupper(buffer[n]);
+		n++;
+	}
+
 	return (buffer);
 }
 
